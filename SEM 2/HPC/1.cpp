332d01da5c2a7ee1d07e311cdc1565bdfeaaee7f
Mustansir Rangwala <119410651+mustansirr@users.noreply.github.com>
2026-01-20 01:12:54 +0530

    start hpc lab 1

--- SEM 2/HPC/1.cpp
+++ SEM 2/HPC/1.cpp
@@ -1,3 +1,17 @@
-#include <opm.h>
+/*
+Design and implement Parallel Breadth First Search and Depth First Search
+based on existing algorithms using OpenMP. Use a Tree or an undirected
+graph for BFS and DFS .
+*/
+
 #include <iostream>
+#include <omp.h>
 using namespace std;
+
+int main() {
+    #pragma omp parallel
+    {
+        printf("Thread: %d\n", omp_get_thread_num());
+    }
+    return 0;
+}
